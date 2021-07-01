fb5bf32eb7ba1efdc253cb70521ea47524bfcddc
maxmayor <64788970+maxmayor@users.noreply.github.com>
2021-07-01 18:39:38 +0100

    Add files via upload

--- contact.php
+++ contact.php
@@ -1,4 +1,36 @@
-﻿<?php include('hnav.php'); ?>
+﻿<?php include('hnav.php'); 
+
+
+     
+
+        print_r($_REQUEST);
+        writeToLog($_REQUEST, 'incoming');
+        
+        /**
+         * Write data to log file.
+         *
+         * @param mixed $data
+         * @param string $title
+         *
+         * @return bool
+         */
+        function writeToLog($data, $title = '') {
+         $log = "\n------------------------\n";
+         $log .= date("Y.m.d G:i:s") . "\n";
+         $log .= (strlen($title) > 0 ? $title : 'DEBUG') . "\n";
+         $log .= print_r($data, 1);
+         $log .= "\n------------------------\n";
+         file_put_contents(getcwd() . '/hook.log', $log, FILE_APPEND);
+         return true;
+        } 
+
+
+      
+
+
+
+
+?>
   
     <div class="site-blocks-cover inner-page-cover overlay" style="background-image: url(images/banner2.png);" data-aos="fade" data-stellar-background-ratio="0.5">
       <div class="container">
